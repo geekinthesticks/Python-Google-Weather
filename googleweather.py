f51262e9e4e667deac140d88b1e69728b737a192
Ian Barton <ian@wilkesley.net>
2010-09-17 15:31:41 +0100

    Added GPL.

--- googleweather.py
+++ googleweather.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python
 
+#   Copyright (C) 2008 Ian Barton.
+##
+##   This program is free software; you can redistribute it and/or modify
+##   it under the terms of the GNU General Public License as published by
+##   the Free Software Foundation; either version 3, or (at your option)
+##   any later version.
+##
+##   This program is distributed in the hope that it will be useful,
+##   but WITHOUT ANY WARRANTY; without even the implied warranty of
+##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##   GNU General Public License for more details.
+
 import urllib, os, sys
 from xml.dom import minidom
 import time
