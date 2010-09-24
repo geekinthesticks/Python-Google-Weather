2da5885a7dab99e14361c082d9f0606dceba435a
Ian Barton <ian@wilkesley.net>
2010-09-24 09:57:20 +0100

    Imported source.

---
+++ example.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# A simple example showing how to use the googleweather class.
+
+from googleweather import *
+
+# Set the location using a post code. Setting "lang=en-gb" will return
+# temperatures in Imperial units. Cache the downloaded data for 60 mins before
+# reloading the data from Google and download the Google weather icons
+# to the current working directory.
+myForecast = googleWeather(location="LL55 4EU", lang="en-gb", expires=60, get_icons=True)
+
+myForecast.setImageLocation("./")
+
+forecast = myForecast.getForecast()
+
+
+print "City: %s Post code: %s" % (forecast['city'][0], forecast['location'])
+for i, day in enumerate(forecast['days']):
+    print "%s: High: %s Low: %s Conditions: %s" % (day, forecast['high'][i], forecast['low'][i], forecast['conditions'][i])
+
